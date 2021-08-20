344c5b23b2803ce5cb5b5aa66fbc2d5d28f6d5ea
Robbe Van Herck <robbe@robbevanherck.be>
2021-08-20 23:50:42 +0200

    Allow multiple types and graphs to be handled

--- app.js
+++ app.js
@@ -1,36 +1,60 @@
+import fs from 'fs';
 import bodyParser from 'body-parser';
+import json from 'express';
 import { app, errorHandler } from 'mu';
 import { filterDeltaForInsertedType } from './lib/delta';
 import selectSubjectsWithoutUuid from './queries/select-without-uuid';
 import insertUuid from './queries/insert-uuid';
 
-const RDF_TYPE = process.env.RDF_TYPE;
-const GRAPH = process.env.GRAPH;
 
-app.post('/run', async (req, res) => {
-  const subjectsWithout = await selectSubjectsWithoutUuid(RDF_TYPE, [GRAPH]);
-  if (subjectsWithout.length > 0) {
-    console.log(`Found ${subjectsWithout.length} objects of type <${RDF_TYPE}> without a uuid. Handling now.`);
-    for (const subject of subjectsWithout) {
-      await insertUuid(subject, [GRAPH]);
+app.use(json());
+
+const CONFIG = readConfig();
+
+function readConfig() {
+  const configText = fs.readFileSync('/config/config.json', { encoding: 'utf8' });
+  const config = JSON.parse(configText);
+
+  return config;
+}
+
+app.post('/run', async (_req, res) => {
+  let uris = new Set();
+
+  for (let [type, graphs] of Object.entries(CONFIG)) {
+    const subjectsWithout = await selectSubjectsWithoutUuid(type, graphs);
+    if (subjectsWithout.length > 0) {
+      console.log(`Found ${subjectsWithout.length} objects of type <${type}> without a uuid. Handling now.`);
+      for (const subject of subjectsWithout) {
+        await insertUuid(subject, graphs);
+        uris.add(subject);
+      }
     }
   }
-  const data = subjectsWithout.map(s => {
-    return { uri: s };
-  });
 
-  return res.send({
-    data
-  });
+  let ret = [];
+
+  uris.forEach((uri) => {
+    ret.push({
+      "uri": uri,
+    })
+  })
+
+  return res.status(200).send(JSON.stringify(
+    ret
+  ));
 });
 
 app.post('/delta', bodyParser.json(), async (req, res) => {
-  res.status(202).end();
-  const insertedSubjects = filterDeltaForInsertedType(req.body, RDF_TYPE);
-  if (insertedSubjects.length > 0) {
-    console.log(`Received ${insertedSubjects.length} object inserts of type <${RDF_TYPE}> through delta's. Handling now.`);
-    for (const subject of insertedSubjects) {
-      await insertUuid(subject, [GRAPH]);
+  res.status(202).send();
+  for (let [type, graphs] of Object.entries(CONFIG)) {
+    console.log(JSON.stringify(req.body))
+    const insertedSubjects = filterDeltaForInsertedType(req.body, type);
+    if (insertedSubjects.length > 0) {
+      console.log(`Received ${insertedSubjects.length} object inserts of type ${type} through deltas. Handling now.`);
+      for (const subject of insertedSubjects) {
+        await insertUuid(subject, graphs);
+      }
     }
   }
 });
