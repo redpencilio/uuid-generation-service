16a5aad37a4a2cfe54210bee52df5e5172ec78b7
felix <ruiz.felixruiz@gmail.com>
2022-04-28 12:47:31 +0200

    Move all processing to the queue

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ import { app, errorHandler } from 'mu';
 import { filterDeltaForInsertedType } from './lib/delta';
 import selectSubjectsWithoutUuid from './queries/select-without-uuid';
 import insertUuid from './queries/insert-uuid';
+import { ProcessingQueue } from './lib/processing-queue';
 import { CronJob } from 'cron';
 import fetch from 'node-fetch';
 
 const CONFIG = readConfig();
+const RUN_CRON_JOBS = process.env.RUN_CRON_JOBS == 'true' || false;
+const CRON_FREQUENCY = process.env.CRON_FREQUENCY || '* */1 * * *';
 
 function readConfig() {
   const configText = fs.readFileSync('/config/config.json', { encoding: 'utf8' });
@@ -16,6 +19,7 @@ function readConfig() {
   return config;
 }
 
+const processingQueue = new ProcessingQueue();
 
 if(RUN_CRON_JOBS) {
   new CronJob(CRON_FREQUENCY, function() {
@@ -25,45 +29,51 @@ if(RUN_CRON_JOBS) {
 }
 
 app.post('/run', async (_req, res) => {
-  let uris = new Set();
 
-  for (let [type, graphs] of Object.entries(CONFIG)) {
-    const subjectsWithout = await selectSubjectsWithoutUuid(type, graphs);
-    if (subjectsWithout.length > 0) {
-      console.log(`Found ${subjectsWithout.length} objects of type <${type}> without a uuid. Handling now.`);
-      for (const subject of subjectsWithout) {
-        await insertUuid(subject, graphs);
-        uris.add(subject);
+  processingQueue.addJob(async () => {
+
+    console.log('Starting processing uuid-generation-job');
+
+    let uris = new Set();
+
+    for (let [type, graphs] of Object.entries(CONFIG)) {
+      const subjectsWithout = await selectSubjectsWithoutUuid(type, graphs);
+      if (subjectsWithout.length > 0) {
+        console.log(`Found ${subjectsWithout.length} objects of type <${type}> without a uuid. Handling now.`);
+        for (const subject of subjectsWithout) {
+          await insertUuid(subject, graphs);
+          uris.add(subject);
+        }
       }
     }
-  }
 
-  let ret = [];
+    console.info(`Finished adding uuid's to the following uris: ${uris.join('\n')}`);
+  });
 
-  uris.forEach((uri) => {
-    ret.push({
-      "uri": uri,
-    })
-  })
-
-  return res.status(200).send(JSON.stringify(
-    {
-      "data": ret
-    }
-  ));
+  return res
+    .status(200)
+    .send(JSON.stringify({"message": "Scheduled uuid-generation-job"}));
 });
 
 app.post('/delta', bodyParser.json(), async (req, res) => {
-  res.status(202).send();
-  for (let [type, graphs] of Object.entries(CONFIG)) {
-    const insertedSubjects = filterDeltaForInsertedType(req.body, type);
-    if (insertedSubjects.length > 0) {
-      console.log(`Received ${insertedSubjects.length} object inserts of type ${type} through deltas. Handling now.`);
-      for (const subject of insertedSubjects) {
-        await insertUuid(subject, graphs);
+
+
+  processingQueue.addJob(async () => {
+
+    for (let [type, graphs] of Object.entries(CONFIG)) {
+      const insertedSubjects = filterDeltaForInsertedType(req.body, type);
+      if (insertedSubjects.length > 0) {
+        console.log(`Received ${insertedSubjects.length} object inserts of type ${type} through deltas. Handling now.`);
+        for (const subject of insertedSubjects) {
+          await insertUuid(subject, graphs);
+        }
       }
     }
-  }
+
+  });
+
+  return  res.status(202).send();
+
 });
 
 app.use(errorHandler);
