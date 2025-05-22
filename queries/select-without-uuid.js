e884f442536c818a5ebd725743c0221b8494d7c5
Erika Pauwels <erika.pauwels@gmail.com>
2025-05-22 10:56:15 +0200

    Ensure selection of all subjects without uuid
    If there are more subjects than the max result size configured in
    Virtuoso only the first batch was returned. Fixed to select all
    subjects now.

--- queries/select-without-uuid.js
+++ queries/select-without-uuid.js
@@ -3,24 +3,48 @@ import { sparqlEscapeUri } from 'mu';
 import { parseSparqlResults } from './util';
 
 async function selectSubjectsWithoutUuid (type, graphs) {
-  const queryString = `PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+  const countQueryResult = await querySudo(`PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
 
-SELECT ?s
+SELECT (COUNT(?s) as ?count)
 WHERE {
     GRAPH ?g {
         ?s a ${sparqlEscapeUri(type)} .
-        FILTER NOT EXISTS { 
-            ?s mu:uuid ?mu_uuid .
+        FILTER NOT EXISTS {
+            ?s mu:uuid ?uuid .
         }
     }
     VALUES ?g {
         ${graphs.map(sparqlEscapeUri).join('\n        ')}
     }
 }
+`);
+
+  const count = parseInt(countQueryResult.results.bindings[0].count.value);
+
+  const batchSize = 10000;
+  const nbOfBatches = Math.ceil(count / batchSize);
+  const subjects = new Set();
+  for (let i = 0; i < nbOfBatches; i++) {
+    const queryString = `PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+SELECT ?s
+WHERE {
+    GRAPH ?g {
+        ?s a ${sparqlEscapeUri(type)} .
+        FILTER NOT EXISTS {
+            ?s mu:uuid ?uuid .
+        }
+    }
+    VALUES ?g {
+        ${graphs.map(sparqlEscapeUri).join('\n        ')}
+    }
+} ORDER BY ?s LIMIT ${batchSize} OFFSET ${i*batchSize}
 `;
-  const res = await querySudo(queryString);
-  const parsedRes = parseSparqlResults(res);
-  return parsedRes.map(res => res.s);
+    const res = await querySudo(queryString);
+    const parsedRes = parseSparqlResults(res);
+    parsedRes.forEach(res => subjects.add(res.s));
+  }
+
+  return [...subjects];
 }
 
 export default selectSubjectsWithoutUuid;
